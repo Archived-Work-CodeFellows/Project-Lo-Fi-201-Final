115d6bfec3d5ee11b34f4169e26ccad73d12532b
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-02-22 20:07:32 -0800

    Added some spacebar functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 //build an audio constructor?
-//needs global variables that will affect variables inside functions
 //event function for spacebar to play audio, switch between the two playing channels
 //needs local storage (For volume and selection)
 
@@ -10,6 +9,7 @@ var chan1 = document.getElementById('channelA');
 var chan2 = document.getElementById('channelB');
 var chanSelector = 0;
 var flagCheck = 0;
+var userVolume = 0.96; //For algorithm purposes
 
 chan1.volume = 0;
 chan2.volume = 0;
@@ -30,7 +30,7 @@ function start () {
   var start = setInterval(function () {
     if(chan1.currentTime < 10 && chan1.volume !== 1) {
       chan1.volume += 0.001;
-      if(chan1.volume > 0.96) {
+      if(chan1.volume > userVolume) {
         chan1.volume = 1;
         chanSelector = 0;
       }
@@ -47,12 +47,12 @@ function channelAfade () {
   if(chan1.currentTime > chan1.duration-10 && !chan1.ended) {
     chan2.play();
     var fade = setInterval(function(){
-      if(chan2.volume < 0.96){
+      if(chan2.volume < userVolume){
         chan1.volume -= 0.001;
         chan2.volume += 0.001;
       } else clearInterval(fade);
     }, 30);
-    if(chan2.volume > 0.96) {
+    if(chan2.volume > userVolume) {
       chan1.volume = 0;
       chan2.volume = 1;
       chanSelector = 0;
@@ -66,12 +66,12 @@ function channelBfade() {
   if(chan2.currentTime > chan2.duration-10 && !chan2.ended) {
     chan1.play();
     var fade = setInterval(function(){
-      if(chan1.volume < 0.96){
+      if(chan1.volume < userVolume){
         chan2.volume -= 0.001;
         chan1.volume += 0.001;
       } else clearInterval(fade);
     }, 30);
-    if(chan1.volume > 0.96) {
+    if(chan1.volume > userVolume) {
       chan2.volume = 0;
       chan1.volume = 1;
       chanSelector = 1;
@@ -81,15 +81,12 @@ function channelBfade() {
   return flagCheck;
 }
 
-document.body.onkeyup = function(event){
-  switch(chanSelector) {
-  case 1:
-    if(event.keyCode === 32 && !chan2.paused) chan2.pause();
-    else if (event.keyCode === 32 && chan2.paused) chan2.play();
-    break;
-  default:
-    if(event.keyCode === 32 && !chan1.paused) chan1.pause();
-    else if (event.keyCode === 32 && chan1.paused) chan1.play();
-    break;
-  }
+document.body.onkeydown = function(event){
+
+  if(event.keyCode === 32 && flagCheck === 1) chan1.pause();
+  //if (event.keyCode === 32 && flagCheck === 1 && chan1.pause() === true) chan1.play();
+  if(event.keyCode === 32 && flagCheck === 2) chan2.pause();
+  //if (event.keyCode === 32 && flagCheck === 2 && chan2.pause() === true) chan2.play();
+
 };
+
