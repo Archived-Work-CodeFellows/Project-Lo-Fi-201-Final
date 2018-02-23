a81825f1333ba9b89681dc68acd0e8450abcf3e8
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-02-22 19:32:00 -0800

    Fixed placeholder variable naming

--- js/placeholder.js
+++ js/placeholder.js
@@ -1,10 +1,10 @@
 function PlaySound(soundobj) {
-    var sound=document.getElementById(soundobj);
-    sound.play();
+  var sound=document.getElementById(soundobj);
+  sound.play();
 }
 
 function StopSound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    sound.pause();
-    sound.currentTime = 0;
+  var sound=document.getElementById(soundobj);
+  sound.pause();
+  sound.currentTime = 0;
 }
