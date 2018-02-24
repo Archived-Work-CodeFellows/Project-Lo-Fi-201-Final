9852323f5a2ffb000df32bc3be97208ab0bcee0b
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-02-24 15:14:44 -0800

    Added audio constructor, chaos functionality, did some about us styling

--- js/app.js
+++ js/app.js
@@ -5,9 +5,13 @@
 //needs local storage (For volume and selection)
 
 var harmonySelector = document.getElementById('harmonic');
+var chaosSelector = document.getElementById('chaotic');
 var chan1 = document.getElementById('channelA');
 var chan2 = document.getElementById('channelB');
+var chan3 = document.getElementById('channelC');
+var chan4 = document.getElementById('channelD');
 var compare = null;
+var compare2 = null;
 Audio_src.all = [];
 var moodSelector = ' ';
 var flagCheck = 0;
@@ -15,8 +19,11 @@ var userVolume = 0.96; //For algorithm purposes
 
 chan1.volume = 0;
 chan2.volume = 0;
+chan3.volume = 0;
+chan4.volume = 0;
 
 harmonySelector.addEventListener('click', start);
+chaosSelector.addEventListener('click', start2);
 
 function Audio_src(trackNum){
   this.chan1 = chan1;
@@ -26,7 +33,7 @@ function Audio_src(trackNum){
   Audio_src.all.push(this);
 }
 for(var i = 1; i < 5; i++) new Audio_src(i);
-
+//Harmony Random
 function setRandomA() {
   console.log('random chan1');
   do {
@@ -45,21 +52,52 @@ function setRandomB() {
   chan2.setAttribute('src', Audio_src.all[indexRandom].harmonyPath);
   return compare = indexRandom;
 }
+//Chaos Random
+function setRandomC() {
+  console.log('random chan3');
+  do {
+    var indexRandom = Math.floor(Math.random()*Audio_src.all.length);
+    console.log(indexRandom);
+  } while (compare2 === indexRandom);
+  chan3.setAttribute('src', Audio_src.all[indexRandom].chaosPath);
+  return compare2 = indexRandom;
+}
+function setRandomD() {
+  console.log('random chan4');
+  do {
+    var indexRandom = Math.floor(Math.random()*Audio_src.all.length);
+    console.log(indexRandom);
+  } while (compare2 === indexRandom);
+  chan4.setAttribute('src', Audio_src.all[indexRandom].chaosPath);
+  return compare2 = indexRandom;
+}
+
 setRandomA();
 setRandomB();
+setRandomC();
+setRandomD();
 
 setInterval(function(){
-  if(flagCheck === 1) {
-    channelAfade();
-  } else if (flagCheck === 2) {
-    channelBfade();
+  if(moodSelector === 'harmony'){
+    if(flagCheck === 1) {
+      channelAfade();
+    } else if (flagCheck === 2) {
+      channelBfade();
+    }
+  } else if (moodSelector === 'chaos') {
+    if(flagCheck === 1) {
+      channelCfade();
+    } else if (flagCheck === 2) {
+      channelDfade();
+    }
   }
 }, 250);
 
 
-
+//Harmony Section
 function start () {
   chan1.play();
+  moodSelector = 'harmony';
   var start = setInterval(function () {
     if(chan1.currentTime < 10 && chan1.volume !== 1) {
       chan1.volume += 0.001;
@@ -72,7 +110,7 @@ function start () {
       clearInterval(start);
     }
   }, 5);
-  return flagCheck;
+  return flagCheck, moodSelector;
 }
 
 function channelAfade () {
@@ -112,6 +150,66 @@ function channelBfade() {
   }
   return flagCheck;
 }
+
+//Chaos Section
+function start2 () {
+  chan3.play();
+  moodSelector = 'chaos';
+  var start = setInterval(function () {
+    if(chan3.currentTime < 10 && chan3.volume !== 1) {
+      chan3.volume += 0.001;
+      if(chan3.volume > userVolume) {
+        chan3.volume = 1;
+      }
+    }
+    if(chan3.volume === 1 || chan3.paused === true) {
+      flagCheck = 1;
+      clearInterval(start);
+    }
+  }, 5);
+  return flagCheck;
+}
+
+function channelCfade () {
+  if(chan3.currentTime > chan3.duration-10 && !chan3.ended) {
+    chan4.play();
+    var fade = setInterval(function(){
+      if(chan4.volume < userVolume){
+        chan3.volume -= 0.001;
+        chan4.volume += 0.001;
+      } else clearInterval(fade);
+    }, 30);
+    if(chan4.volume > userVolume) {
+      chan3.volume = 0;
+      chan4.volume = 1;
+      setRandomC();
+      flagCheck = 2;
+    }
+    return flagCheck;
+  }
+}
+
+function channelDfade() {
+  if(chan4.currentTime > chan4.duration-10 && !chan4.ended) {
+    chan3.play();
+    var fade = setInterval(function(){
+      if(chan3.volume < userVolume){
+        chan4.volume -= 0.001;
+        chan3.volume += 0.001;
+      } else clearInterval(fade);
+    }, 30);
+    if(chan3.volume > userVolume) {
+      chan4.volume = 0;
+      chan3.volume = 1;
+      setRandomD();
+      flagCheck = 1;
+    }
+  }
+  return flagCheck;
+}
+
+
+
 
 document.body.onkeydown = function(event){
 
