40c41cd789e5a7d04fe30acf05d15f816f3a6265
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-02-27 21:36:37 -0800

    Started refactoring of js

--- js/app.js
+++ js/app.js
@@ -7,75 +7,45 @@ var harmonySelector = document.getElementById('playHarmony');
 var chaosSelector = document.getElementById('playChaos');
 var slider = document.getElementById('volume');
 var playButton = document.getElementById('play');
-var userVolume = 0;
-var userToggle = false;
 var chan1 = document.getElementById('channelA');
 var chan2 = document.getElementById('channelB');
 var chan3 = document.getElementById('channelC');
 var chan4 = document.getElementById('channelD');
+var userVolume = 0;
+var userToggle = false;
 var compare = null;
-var compare2 = null;
 var activeChannel = null;
 Audio_src.all = [];
 var moodSelector = ' ';
 var flagCheck = 0;
-//var userVolume = 0.96; //For algorithm purposes
-
-chan1.volume = 0;
-chan2.volume = 0;
-chan3.volume = 0;
-chan4.volume = 0;
 
 harmonySelector.addEventListener('click', start);
 chaosSelector.addEventListener('click', startChaos);
 
 function Audio_src(trackNum){
-  this.chan1 = chan1;
-  this.chan2 = chan2;
   this.harmonyPath = 'audio/harmony/harmony_'+trackNum+'.mp3';
   this.chaosPath = 'audio/chaos/chaos_'+trackNum+'.mp3';
   Audio_src.all.push(this);
 }
-for(var i = 1; i < 5; i++) new Audio_src(i);
-//Harmony Random
-function setRandomA() {
+function setRandom() {
   do {
     var indexRandom = Math.floor(Math.random()*Audio_src.all.length);
-
+    console.log('dupes');
   } while (compare === indexRandom);
-  chan1.setAttribute('src', Audio_src.all[indexRandom].harmonyPath);
   return compare = indexRandom;
 }
-function setRandomB() {
-  do {
-    var indexRandom = Math.floor(Math.random()*Audio_src.all.length);
-
-  } while (compare === indexRandom);
-  chan2.setAttribute('src', Audio_src.all[indexRandom].harmonyPath);
-  return compare = indexRandom;
+function initialize() {
+  chan1.volume = 0;
+  chan2.volume = 0;
+  chan3.volume = 0;
+  chan4.volume = 0;
+  for(var i = 1; i < 5; i++) new Audio_src(i);
+  chan1.setAttribute('src', Audio_src.all[setRandom()].harmonyPath);
+  chan2.setAttribute('src', Audio_src.all[setRandom()].harmonyPath);
+  chan3.setAttribute('src', Audio_src.all[setRandom()].chaosPath);
+  chan4.setAttribute('src', Audio_src.all[setRandom()].chaosPath);
 }
-//Chaos Random
-function setRandomC() {
-  do {
-    var indexRandom = Math.floor(Math.random()*Audio_src.all.length);
-
-  } while (compare2 === indexRandom);
-  chan3.setAttribute('src', Audio_src.all[indexRandom].chaosPath);
-  return compare2 = indexRandom;
-}
-function setRandomD() {
-  do {
-    var indexRandom = Math.floor(Math.random()*Audio_src.all.length);
-
-  } while (compare2 === indexRandom);
-  chan4.setAttribute('src', Audio_src.all[indexRandom].chaosPath);
-  return compare2 = indexRandom;
-}
-
-setRandomA();
-setRandomB();
-setRandomC();
-setRandomD();
+initialize();
 
 function playing() {
   if(activeChannel.paused === true) playButton.onclick = activeChannel.play();
@@ -110,7 +80,6 @@ setInterval(function(){
     }
   }
 }, 1);
-
 
 //Harmony Section
 function start () {
@@ -153,7 +122,7 @@ function channelAfade () {
         }
       }
       if(chan2.volume === volumeStore) {
-        setRandomA();
+        chan1.setAttribute('src', Audio_src.all[setRandom()].harmonyPath);
         userToggle = true;
         playButton.addEventListener('click', playing);
         clearInterval(fade);
@@ -181,7 +150,7 @@ function channelBfade() {
         }
       }
       if(chan1.volume === userVolume) {
-        setRandomB();
+        chan2.setAttribute('src', Audio_src.all[setRandom()].harmonyPath);
         userToggle = true;
         playButton.addEventListener('click', playing);
         clearInterval(fade);
@@ -232,7 +201,7 @@ function channelCfade () {
         }
       }
       if(chan4.volume === volumeStore) {
-        setRandomC();
+        chan3.setAttribute('src', Audio_src.all[setRandom()].chaosPath);
         userToggle = true;
         playButton.addEventListener('click', playing);
         clearInterval(fade);
@@ -260,7 +229,7 @@ function channelDfade() {
         }
       }
       if(chan3.volume === userVolume) {
-        setRandomD();
+        chan4.setAttribute('src', Audio_src.all[setRandom()].chaosPath);
         userToggle = true;
         playButton.addEventListener('click', playing);
         clearInterval(fade);
