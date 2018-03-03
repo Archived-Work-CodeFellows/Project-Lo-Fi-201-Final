db6b37e37ae6c51d6274951418f1d0efdf9cd6ad
Bryan Brinson <fdfd@beb.com>
2018-03-02 22:29:10 -0800

    min and max change to index

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 'use strict';
-// debugger;
+ debugger;
 
 document.getElementById('home').addEventListener('click', function () {
   window.location.href = 'index.html';
@@ -14,26 +14,33 @@ var playChaosBtn = document.getElementById('playChaos');
 var harmonyListItem = document.getElementById('harmonyListItem');
 var chaosListItem = document.getElementById('chaosListItem');
 
+
+
 playHarmonyBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  if (harmonyListItem.classList.contains('minimized')) {
-    harmonyListItem.classList.toggle('minimized',false);
-    harmonyListItem.classList.toggle('maximized',true);
-  }
-  if (chaosListItem.classList.contains('maximized')) {
-    chaosListItem.classList.toggle('minimized',true);
+
+  if (harmonyListItem.classList.contains('maximized')) {
+    harmonyListItem.classList.toggle('maximized',false);
     chaosListItem.classList.toggle('maximized',false);
+    document.getElementById('playHarmony').textContent = 'Play this mood';
+  } else {
+    harmonyListItem.classList.toggle('maximized',true);
+    document.getElementById('playHarmony').textContent = '(minimize)';
+    chaosListItem.classList.toggle('maximized',false);
+    document.getElementById('playChaos').textContent = 'Play this mood';
   }
 });
 
 playChaosBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  if (chaosListItem.classList.contains('minimized')) {
-    chaosListItem.classList.toggle('minimized',false);
-    chaosListItem.classList.toggle('maximized',true);
-  }
-  if (harmonyListItem.classList.contains('maximized')) {
-    harmonyListItem.classList.toggle('minimized',true);
+  if (chaosListItem.classList.contains('maximized')) {
     harmonyListItem.classList.toggle('maximized',false);
+    chaosListItem.classList.toggle('maximized',false);
+    document.getElementById('playChaos').textContent = 'Play this mood';
+  } else {
+    harmonyListItem.classList.toggle('maximized',false);
+    document.getElementById('playHarmony').textContent = 'Play this mood';
+    chaosListItem.classList.toggle('maximized',true);
+    document.getElementById('playChaos').textContent = '(minimize)';
   }
 });
